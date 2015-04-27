dfc0d5ac995693d42d8d748b3b1d7b4c0fc492bc
oj.bae <oj.bae@hanmail.net>
2015-04-27 18:57:58 +0900

    register src

--- src/main/java/com/test/controller/MybatisController.java
+++ src/main/java/com/test/controller/MybatisController.java
@@ -20,32 +20,26 @@ import com.test.mybatis.MemberDAOService;
 @Controller
 public class MybatisController {
 	
-	// 
+
 	@Autowired
 	private MemberDAOService memberDAOService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(MybatisController.class);
-	
-	
-	//시작 메인화면.
+
 	@RequestMapping("/main")
 	public ModelAndView main(Locale locale, Model model) {
 		logger.info("Welcome main.", locale);
-
-		// view 화면인 main.jsp에 DB로부터 읽어온 데이터를 보여준다.
 		ModelAndView result = new ModelAndView();
-		//addObject view에 넘어가는 데이터
 		List<Member> memberList = memberDAOService.getMembers();
 		result.addObject("result", memberList);
 		result.setViewName("main");
 		return result;
 	}
-	
-	//insert 버튼 클릭시 값을 가져와서 result.jsp로 화면전환 해준다.
+
+
 	@RequestMapping(value ="/insert", method = RequestMethod.POST)
 	public ModelAndView insert(HttpServletRequest request){
-		
-		// HttpServletRequest를 이용하여 main.jsp로부터 
+
 		Member member = new Member();
 		member.set_name((String) request.getParameter("name"));
 		member.set_email((String) request.getParameter("email"));
@@ -53,8 +47,7 @@ public class MybatisController {
 		
 		memberDAOService.insertMember(member);
 		System.out.println("insert complet");
-		
-		//아래부분은 select값을 result.jsp파일에 보여주기 위해 또사용.
+
 		ModelAndView result = new ModelAndView();
 		List<Member> memberList = memberDAOService.getMembers();
 		result.addObject("result", memberList);
